e63b1ded651d3e5d9a6c90a51b0e64e8ed4dfe0d
Kevin Ostafinski <kevinostafinski@gmail.com>
2021-02-19 23:01:46 +0100

    moved db.json to seperate folder

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const router = jsonServer.router('api/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+// server.use((req, res, next) => {
+//   res.header('Content-Range', 'posts 0-20/20')
+//   next()
+// })
 server.use(router)
 server.use(jsonServer.bodyParser)
 
